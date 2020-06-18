559808f6889ec53867e75f38456cc1ec096b5aac
unknown <aayu.shah711@gmail.com>
2020-06-18 10:40:58 +0530

    Modifies expenses page: Added javascript to display expense cards & total

--- expenses.js
+++ expenses.js
@@ -1,3 +1,4 @@
+var balance = 0
 var currentUser = localStorage.getItem("current-user")
 if (currentUser){
     var users = JSON.parse(localStorage.getItem("users"))
@@ -10,8 +11,50 @@ else {
 
 function updateDayExpenses(){
     var dayExpenses = document.querySelector("#dayExpenses")
-    dayExpenses.textContent = new Date().toDateString() + " : ₹" + user.balance
+    dayExpenses.textContent = new Date().toDateString() + " : ₹" + balance
     console.log("updating date...")
+}
+
+function expenseCards(){
+    var expenseCards = document.querySelector("#expenseCards")
+    var date = "2020-06-18" // Add logic here
+    var expensesForDay = user.expenses[date]
+    for (var i=0;i<expensesForDay.length;i++){
+        var expense = expensesForDay[i]
+        var card = createCard(expense.amount,expense.category,expense.notes)
+        balance += Number(expense.amount)
+        expenseCards.append(card)
+    }
+    
+    function createCard(amount,category,notes){
+        var h5 = createTag("h5",{"class":"mt-0"},category)
+        var mediaBody = createTag("div",{"class":"media-body"})
+        mediaBody.append(h5,notes)
+        var media = createTag("div", {"class":"media"})
+        var pic = createTag("i",{"class":"fas fa-car mr-3"})
+        media.append(pic,mediaBody)
+        var col10 = createTag("div", {"class":"col-10"})
+        col10.append(media)
+        var h4 = createTag("h4",{"class":"text-danger"},"₹"+amount)
+        var col2 = createTag("div", {"class":"col-2 d-flex justify-content-center align-items-top"})
+        col2.append(h4)
+        var row = createTag("div", {"class":"row m-0"})
+        row.append(col10,col2)
+        var cardBody = createTag("div", {"class":"card-body"})
+        cardBody.append(row)
+        var card = createTag("div", {"class":"card m-2"})
+        card.append(cardBody)
+        return card
+    }
+}
+
+function createTag(tag,attributes,content){
+    var tag = document.createElement(tag)
+    for (key in attributes){
+        tag.setAttribute(key,attributes[key])
+    }
+    tag.textContent = content
+    return tag
 }
 
 function addExpense(){
@@ -28,15 +71,18 @@ function addExpense(){
     }
     else {
         console.log("expenses don't exist")
-        user.expenses[expense.date] = {}
+        user.expenses[expense.date] = []
     }
 
-    user.expenses[expense.date][expense.date.length] = expense
+    console.log(expense.date.length)
+    user.expenses[expense.date].push(expense)
     users[currentUser] = user
 
     localStorage.setItem("users",JSON.stringify(users))
+    location.href = "expenses.html"
 }
 
 window.onload = function(){
+    expenseCards()
     updateDayExpenses()
 }

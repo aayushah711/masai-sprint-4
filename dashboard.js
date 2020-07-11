dca622bc84d9e2642247590ba36402e10337558a
unknown <aayu.shah711@gmail.com>
2020-07-11 17:02:43 +0530

    Changed the format of date for storing data in local storage

--- dashboard.js
+++ dashboard.js
@@ -1,55 +1,48 @@
-var currentUser = localStorage.getItem("current-user")
-if (currentUser){
-    var users = JSON.parse(localStorage.getItem("users"))
-    var user = users[currentUser] 
-}
-else {
-    alert("Please login!")
-    location.href = "login.html"
-}
-
-function updateData(){
-    var date = "2020-06-18"
-    if (user["balance"][date]){
-        var expenses = Number(user["balance"][date]["expenses"])
-        var balance = Number(user["balance"][date]["balance"])
-    }
-    else {
-        var expenses = 0
-        var balance = 0
-    }
-
-    var expensesProgress = document.querySelector("#expensesProgress")
-    expensesProgress.textContent = "Expenses ₹" + expenses
-
-    var balanceProgress = document.querySelector("#balanceProgress")
-    var balanceCard = document.querySelector("#balanceCard")
-    var balanceNav = document.querySelector("#balanceNav")
-    console.log(balanceNav)
-    
-    if (balance<0){
-        balanceProgress.textContent = "Balance: -₹" + -balance
-        balanceProgress.setAttribute("class","text-danger")
-        balanceCard.textContent = "-₹" + -balance
-        balanceNav.textContent = "-₹" + -balance
-        balanceNav.setAttribute("class","my-0 text-danger")
-    }
-    else {
-        balanceProgress.textContent = "Balance ₹" + balance
-        balanceCard.textContent = "₹" + balance
-        balanceNav.textContent = "₹" + balance
-    }
-
-    var expensesCard = document.querySelector("#expensesCard")
-    expensesCard.textContent = "₹" + expenses
-
-
-    expensesProgress.parentElement.nextElementSibling.firstElementChild.style = "width: "+(expenses/2000)*100+"%"
+var currentUser = localStorage.getItem('current-user');
+if (currentUser) {
+	var users = JSON.parse(localStorage.getItem('users'));
+	var user = users[currentUser];
+} else {
+	alert('Please login!');
+	location.href = 'login.html';
 }
 
+function updateData() {
+	var date = new Date().toDateString();
+	if (user['balance'][date]) {
+		var expenses = Number(user['balance'][date]['expenses']);
+		var balance = Number(user['balance'][date]['balance']);
+	} else {
+		var expenses = 0;
+		var balance = 0;
+	}
 
+	var expensesProgress = document.querySelector('#expensesProgress');
+	expensesProgress.textContent = 'Expenses ₹' + expenses;
 
+	var balanceProgress = document.querySelector('#balanceProgress');
+	var balanceCard = document.querySelector('#balanceCard');
+	var balanceNav = document.querySelector('#balanceNav');
+	console.log(balanceNav);
 
-window.onload = function(){
-    updateData()
+	if (balance < 0) {
+		balanceProgress.textContent = 'Balance: -₹' + -balance;
+		balanceProgress.setAttribute('class', 'text-danger');
+		balanceCard.textContent = '-₹' + -balance;
+		balanceNav.textContent = '-₹' + -balance;
+		balanceNav.setAttribute('class', 'my-0 text-danger');
+	} else {
+		balanceProgress.textContent = 'Balance ₹' + balance;
+		balanceCard.textContent = '₹' + balance;
+		balanceNav.textContent = '₹' + balance;
+	}
+
+	var expensesCard = document.querySelector('#expensesCard');
+	expensesCard.textContent = '₹' + expenses;
+
+	expensesProgress.parentElement.nextElementSibling.firstElementChild.style = 'width: ' + expenses / 2000 * 100 + '%';
 }
+
+window.onload = function() {
+	updateData();
+};
--- expenses.js
+++ expenses.js
@@ -1,209 +1,225 @@
-"use strict";
+'use strict';
 
-var BASE_URL = "expenses.html"
-var activeDate
+var BASE_URL = 'expenses.html';
+var activeDate;
 
 function getActiveDate() {
-    var params = new URLSearchParams(document.location.search)
-    activeDate = params.get("activeDate")
-    if (activeDate === null){
-        activeDate = new Date()
-    }
-    else {
-        activeDate = new Date(activeDate)
-    }
+	var params = new URLSearchParams(document.location.search);
+	activeDate = params.get('activeDate');
+	if (activeDate === null) {
+		activeDate = new Date();
+	} else {
+		activeDate = new Date(activeDate);
+	}
 }
 
-var totalExpenseForDay = 0 // Initialised total expenses for the day with 0
-var currentUser = localStorage.getItem("current-user")
-if (currentUser){ // If current user is available: fetch its data
-    var users = JSON.parse(localStorage.getItem("users"))
-    var user = users[currentUser] 
-}
-else { // Else ask to login
-    alert("Please login!")
-    location.href = "login.html"
+var totalExpenseForDay = 0; // Initialised total expenses for the day with 0
+var currentUser = localStorage.getItem('current-user');
+if (currentUser) {
+	// If current user is available: fetch its data
+	var users = JSON.parse(localStorage.getItem('users'));
+	var user = users[currentUser];
+} else {
+	// Else ask to login
+	alert('Please login!');
+	location.href = 'login.html';
 }
 
-// On load display : 
-// 1. expense cards 
+function updateNavData() {
+	var date = new Date().toDateString();
+	if (user['balance'][date]) {
+		var balance = Number(user['balance'][date]['balance']);
+	} else {
+		var balance = 0;
+	}
+
+	var balanceNav = document.querySelector('#balanceNav');
+
+	if (balance < 0) {
+		balanceNav.textContent = '-₹' + -balance;
+		balanceNav.setAttribute('class', 'my-0 text-danger');
+	} else {
+		balanceNav.textContent = '₹' + balance;
+	}
+}
+
+// On load display :
+// 1. expense cards
 // 2. total expenses for the day in rupees on the gray navbar
-window.onload = function(){
-    getActiveDate()
-    expenseCards()
-    updateDayExpenses()
-    updateModalDate()
+window.onload = function() {
+	updateNavData();
+	getActiveDate();
+	expenseCards();
+	updateDayExpenses();
+	updateModalDate();
+};
+
+function expenseCards() {
+	var expenseCards = document.querySelector('#expenseCards');
+	expenseCards.innerHTML = '';
+	// var activeDate = "2020-06-18" // Add logic here
+	var expensesForDay = user.expenses[activeDate.toDateString()];
+	if (expensesForDay) {
+		for (var i = 0; i < expensesForDay.length; i++) {
+			var expense = expensesForDay[i];
+			var card = createCard(expense.amount, expense.category, expense.notes);
+			totalExpenseForDay += Number(expense.amount);
+			expenseCards.append(card);
+		}
+	}
+
+	userBalance();
+
+	function createCard(amount, category, notes) {
+		var h5 = createTag('h5', { class: 'mt-0' }, category);
+		var mediaBody = createTag('div', { class: 'media-body' });
+		mediaBody.append(h5, notes);
+		var media = createTag('div', { class: 'media' });
+		// var pic = createTag("i",{"class":"fas fa-arrow-right mr-3"})
+		var pic = createTag('i', { class: 'fas fa-' + getPic(category) + ' mr-3' });
+		media.append(pic, mediaBody);
+		var col10 = createTag('div', { class: 'col-10' });
+		col10.append(media);
+		var h4 = createTag('h4', { class: 'text-danger' }, '₹' + amount);
+		var col2 = createTag('div', { class: 'col-2 d-flex justify-content-center align-items-top' });
+		col2.append(h4);
+		var row = createTag('div', { class: 'row m-0' });
+		row.append(col10, col2);
+		var cardBody = createTag('div', { class: 'card-body' });
+		cardBody.append(row);
+		var card = createTag('div', { class: 'card m-2' });
+		card.append(cardBody);
+		return card;
+	}
+
+	function getPic(category) {
+		switch (category) {
+			case 'Automobile':
+				return 'car-alt bg-primary';
+			case 'Entertainment':
+				return 'theater-masks bg-secondary';
+			case 'Family':
+				return 'child bg-success';
+			case 'Food':
+				return 'utensils bg-danger';
+			case 'Petrol':
+				return 'gas-pump bg-warning';
+			case 'Gifts':
+				return 'gift bg-info';
+			case 'Groceries':
+				return 'shopping-cart bg-dark';
+			case 'Health':
+				return 'running bg-primary';
+			case 'Housing':
+				return 'house-user bg-secondary';
+			case 'Medical':
+				return 'medkit bg-success';
+			case 'Parking':
+				return 'parking bg-danger';
+			case 'Shopping':
+				return 'shopping-bag bg-warning';
+			case 'Utilities':
+				return 'bolt bg-info';
+		}
+		return 'arrow-right';
+	}
 }
 
-function expenseCards(){
-    var expenseCards = document.querySelector("#expenseCards")
-    expenseCards.innerHTML = ""
-    // var activeDate = "2020-06-18" // Add logic here
-    var expensesForDay = user.expenses[activeDate.toDateString()]
-    if (expensesForDay){
-        for (var i=0;i<expensesForDay.length;i++){
-            var expense = expensesForDay[i]
-            var card = createCard(expense.amount,expense.category,expense.notes)
-            totalExpenseForDay += Number(expense.amount)
-            expenseCards.append(card)
-        }
-    }
-
-    userBalance()
-
-    
-    function createCard(amount,category,notes){
-        var h5 = createTag("h5",{"class":"mt-0"},category)
-        var mediaBody = createTag("div",{"class":"media-body"})
-        mediaBody.append(h5,notes)
-        var media = createTag("div", {"class":"media"})
-        // var pic = createTag("i",{"class":"fas fa-arrow-right mr-3"})
-        var pic = createTag("i",{"class": "fas fa-"+getPic(category)+" mr-3"})
-        media.append(pic,mediaBody)
-        var col10 = createTag("div", {"class":"col-10"})
-        col10.append(media)
-        var h4 = createTag("h4",{"class":"text-danger"},"₹"+amount)
-        var col2 = createTag("div", {"class":"col-2 d-flex justify-content-center align-items-top"})
-        col2.append(h4)
-        var row = createTag("div", {"class":"row m-0"})
-        row.append(col10,col2)
-        var cardBody = createTag("div", {"class":"card-body"})
-        cardBody.append(row)
-        var card = createTag("div", {"class":"card m-2"})
-        card.append(cardBody)
-        return card
-    }
-
-    function getPic(category){
-        switch (category){
-            case "Automobile":
-                return "car-alt bg-primary"
-            case "Entertainment":
-                return "theater-masks bg-secondary"
-            case "Family":
-                return "child bg-success"
-            case "Food":
-                return "utensils bg-danger"
-            case "Petrol":
-                return "gas-pump bg-warning"
-            case "Gifts":
-                return "gift bg-info"
-            case "Groceries":
-                return "shopping-cart bg-dark"
-            case "Health":
-                return "running bg-primary"
-            case "Housing":
-                return "house-user bg-secondary"
-            case "Medical":
-                return "medkit bg-success"
-            case "Parking":
-                return "parking bg-danger"
-            case "Shopping":
-                return "shopping-bag bg-warning"
-            case "Utilities":
-                return "bolt bg-info"
-        }
-        return "arrow-right"
-    }
-}
-
-function userBalance(){
-    // var activeDate = "2020-06-18" // Add logic here
-    console.log(activeDate)
-    if (user.balance[activeDate.toDateString()]){
-        user.balance[activeDate.toDateString()]["expenses"] = totalExpenseForDay
-    }
-    else {
-        console.log("balances don't exist")
-        user.balance[activeDate.toDateString()] = {
-            "date": activeDate.toDateString(),
-            "expenses": totalExpenseForDay,
-            "incomes": 0,
-        }
-    }
-    user.balance[activeDate.toDateString()]["balance"] = Number(user.balance[activeDate.toDateString()]["incomes"]) - Number(user.balance[activeDate.toDateString()]["expenses"])
-    localStorage.setItem("users",JSON.stringify(users))
+function userBalance() {
+	// var activeDate = "2020-06-18" // Add logic here
+	console.log(activeDate);
+	if (user.balance[activeDate.toDateString()]) {
+		user.balance[activeDate.toDateString()]['expenses'] = totalExpenseForDay;
+	} else {
+		console.log("balances don't exist");
+		user.balance[activeDate.toDateString()] = {
+			date: activeDate.toDateString(),
+			expenses: totalExpenseForDay,
+			incomes: 0
+		};
+	}
+	user.balance[activeDate.toDateString()]['balance'] =
+		Number(user.balance[activeDate.toDateString()]['incomes']) -
+		Number(user.balance[activeDate.toDateString()]['expenses']);
+	localStorage.setItem('users', JSON.stringify(users));
 }
 
 // returns a tag with required attributes & content (Eg. div tag with class="card")
-function createTag(tag,attributes,content){
-    var tag = document.createElement(tag)
-    for (var key in attributes){
-        tag.setAttribute(key,attributes[key])
-    }
-    tag.textContent = content
-    return tag
+function createTag(tag, attributes, content) {
+	var tag = document.createElement(tag);
+	for (var key in attributes) {
+		tag.setAttribute(key, attributes[key]);
+	}
+	tag.textContent = content;
+	return tag;
 }
 
 // updates the gray navbar with total expenses for the day in rupees
-function updateDayExpenses(){
-    var dayExpenses = document.querySelector("#dayExpenses")
-    dayExpenses.textContent = activeDate.toDateString() + " : ₹" + totalExpenseForDay
-    console.log("updating date...")
+function updateDayExpenses() {
+	var dayExpenses = document.querySelector('#dayExpenses');
+	dayExpenses.textContent = activeDate.toDateString() + ' : ₹' + totalExpenseForDay;
+	console.log('updating date...');
 }
 
 // Go to the previous day page
-function prevDayExpense(){
-    activeDate.setDate(activeDate.getDate()-1)
-    refreshPage()
+function prevDayExpense() {
+	activeDate.setDate(activeDate.getDate() - 1);
+	refreshPage();
 }
 
 // Go to the next day page
-function nextDayExpense(){
-    activeDate.setDate(activeDate.getDate()+1)
-    refreshPage()
+function nextDayExpense() {
+	activeDate.setDate(activeDate.getDate() + 1);
+	refreshPage();
 }
 
-function refreshPage(){
-    var params = new URLSearchParams();
-    params.set("activeDate",activeDate.toDateString())
-    location = BASE_URL+"?"+params
+function refreshPage() {
+	var params = new URLSearchParams();
+	params.set('activeDate', activeDate.toDateString());
+	location = BASE_URL + '?' + params;
 }
 
 // Gives a string for today's date in the format yyyy-mm-dd (Eg. "2020-06-18")
-function todaysDate(date=new Date()) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+function todaysDate(date = new Date()) {
+	var d = new Date(date),
+		month = '' + (d.getMonth() + 1),
+		day = '' + d.getDate(),
+		year = d.getFullYear();
 
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
+	if (month.length < 2) month = '0' + month;
+	if (day.length < 2) day = '0' + day;
 
-    return [year, month, day].join('-');
+	return [ year, month, day ].join('-');
 }
 
 // Enables user to add a new expense item & stores that data
-function addExpense(){
-    console.log("Adding expense")
-    var expense = {}
-    expense.amount = document.querySelector("#amount").value
-    expense.date = new Date(document.querySelector("#date").value)
-    expense.date = expense.date.toDateString()
-    console.log(expense.date)
-    expense.category = document.querySelector("#category").value
-    expense.notes = document.querySelector("#notes").value
-    
-    if (user.expenses[expense.date]){
-        console.log("expenses exist")
-    }
-    else {
-        console.log("expenses don't exist")
-        user.expenses[expense.date] = []
-    }
+function addExpense() {
+	console.log('Adding expense');
+	var expense = {};
+	expense.amount = document.querySelector('#amount').value;
+	expense.date = new Date(document.querySelector('#date').value);
+	expense.date = expense.date.toDateString();
+	console.log(expense.date);
+	expense.category = document.querySelector('#category').value;
+	expense.notes = document.querySelector('#notes').value;
 
-    console.log(expense.date.length)
-    user.expenses[expense.date].push(expense)
-    users[currentUser] = user
+	if (user.expenses[expense.date]) {
+		console.log('expenses exist');
+	} else {
+		console.log("expenses don't exist");
+		user.expenses[expense.date] = [];
+	}
 
-    localStorage.setItem("users",JSON.stringify(users))
-    location.href = "expenses.html"
+	console.log(expense.date.length);
+	user.expenses[expense.date].push(expense);
+	users[currentUser] = user;
+
+	localStorage.setItem('users', JSON.stringify(users));
+	location.href = 'expenses.html';
 }
 
 // Changes the placeholder date to date for that day inside the modal form
-function updateModalDate(){
-    var date = document.getElementById("date")
-    date.value = todaysDate(activeDate)
+function updateModalDate() {
+	var date = document.getElementById('date');
+	date.value = todaysDate(activeDate);
 }
